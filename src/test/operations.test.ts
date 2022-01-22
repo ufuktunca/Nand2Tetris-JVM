93083078f70e845a7ad7527bfb0623025dd5eecd
ufuk barış tunca <ufukbaristunca@windowslive.com>
2022-01-23 00:20:19 +0300

    test file changed

--- src/test/operations.test.ts
+++ src/test/operations.test.ts
@@ -3,17 +3,13 @@ import { operations } from "../operations/operations";
 
 describe("Given want I decide to operation", () => {
   context("When calling operation function", () => {
+    const result = operations("push constants 15");
     it("Then should return push assembly code", () => {
-      assert.equal(
-        operations("push constants 15"),
-        `@${15}\nD=A\n@0\nA=M\nM=D\n@0\nM=M+1\n`
-      );
+      assert.equal(result, `@${15}\nD=A\n@0\nA=M\nM=D\n@0\nM=M+1\n`);
     });
+    const result2 = operations("add");
     it("Then should return push assembly code", () => {
-      assert.equal(
-        operations("add"),
-        `@0\nA=M\nA=A-1\nD=M\nA=A-1\nM=M+D\n@0\nM=M-1\n`
-      );
+      assert.equal(result2, `@0\nA=M\nA=A-1\nD=M\nA=A-1\nM=M+D\n@0\nM=M-1\n`);
     });
   });
 });
